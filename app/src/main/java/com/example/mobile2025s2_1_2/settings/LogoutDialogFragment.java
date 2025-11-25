ea28d45005104ebbe697bc16bd61461e897f5115
신윤솔 <yunsol@sin-yunsol-ui-MacBookAir.local>
2025-11-25 20:08:09 +0900

    계정삭제 및 로그아웃 구현

--- app/src/main/java/com/example/mobile2025s2_1_2/settings/LogoutDialogFragment.java
+++ app/src/main/java/com/example/mobile2025s2_1_2/settings/LogoutDialogFragment.java
@@ -1,6 +1,10 @@
 package com.example.mobile2025s2_1_2.settings;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.mobile2025s2_1_2.R;
+import com.example.mobile2025s2_1_2.login.StartActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LogoutDialogFragment extends Fragment {
 
@@ -18,7 +24,32 @@ public class LogoutDialogFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        //firebase
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        SharedPreferences prefs = getActivity().getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+        String myemail = prefs.getString("user_email", null);
+
         View view = inflater.inflate(R.layout.settings_logout_dialog, container, false);
+
+        TextView delete = view.findViewById(R.id.logout_ok);
+        delete.setOnClickListener(v ->{
+            SettingsFragment.touchBlocker.setVisibility(View.GONE);
+            // 🔥 다이얼로그 종료
+            requireActivity().getSupportFragmentManager().popBackStack();
+
+            // 🔥 SharedPreferences 초기화 (자동 로그아웃)
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.clear();
+            editor.apply();
+
+            // 🔥 로그인 화면으로 이동
+            Intent intent = new Intent(requireActivity(), StartActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            requireActivity().finish();
+        });
+
+        //취소 버튼
         TextView logoutCancel = view.findViewById(R.id.logout_cancel);
         logoutCancel.setOnClickListener(v -> {
             SettingsFragment.touchBlocker.setVisibility(View.GONE);
