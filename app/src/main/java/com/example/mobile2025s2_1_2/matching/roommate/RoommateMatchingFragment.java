c28cedab6978bb74012eb83a2931a7c8c4ec5676
ykk11-ship-it <ykk11@kookmin.ac.kr>
2025-11-25 18:18:01 +0900

    notification_listener_modification

--- app/src/main/java/com/example/mobile2025s2_1_2/matching/roommate/RoommateMatchingFragment.java
+++ app/src/main/java/com/example/mobile2025s2_1_2/matching/roommate/RoommateMatchingFragment.java
@@ -81,7 +81,7 @@ public class RoommateMatchingFragment extends Fragment {
             RoommateMatchingSuccessFragment fragment = new RoommateMatchingSuccessFragment();
             Bundle args = new Bundle();
             args.putString("name", name);
-            args.putString("email", email);
+            args.putString("email", toID);
             fragment.setArguments(args);
             requireActivity().getSupportFragmentManager()
                     .beginTransaction()
